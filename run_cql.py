497b6f1944b53725f850d7a4612f2ac3631f70a8
Jianhai Su <suj@email.sc.edu>
2023-06-18 23:08:44 -0400

    add two scripts for running experiments on Atari games

--- run_cql.py
+++ run_cql.py
@@ -1,38 +1,79 @@
+import random
 import d3rlpy
-#from sklearn.model_selection import train_test_split
+import numpy as np
+import torch
 
-# prepare dataset
-dataset, env = d3rlpy.datasets.get_atari('breakout-medium-v0') #('breakout-expert-v0') # mixed, medium, expert; Qbert, Pong, Breakout, Seaquest, ASTERIX
-print(f"observation shape: {dataset.observations.shape}")
+def set_seed(seed, env=None):
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    if env is not None:
+        env.seed(seed)
 
+def main(args):
 
-# split dataset
-#train_episodes, test_episodes = train_test_split(dataset, test_size=0.1) #0.1
-train_episodes = dataset.episodes
-test_episodes = [train_episodes[0]]
+    # prepare dataset
+    dataset, env = d3rlpy.datasets.get_atari(args.env_name) #('breakout-expert-v0') # mixed, medium, expert; Qbert, Pong, Breakout, Seaquest, ASTERIX
+    print(f"observation shape: {dataset.observations.shape}")
 
-# prepare algorithm
-cql = d3rlpy.algos.DiscreteCQL(
-    n_frames=4,
-    batch_size=256,
-    target_update_interval=5000,
-    q_func_factory='qr',
-    scaler='pixel',
-    use_gpu=True,
-)
+    train_episodes = dataset.episodes
+    test_episodes = [train_episodes[0]]
 
-# start training
-cql.fit(
-    train_episodes,
-    eval_episodes=test_episodes, # in order to trigger evaluations
-    #n_epochs=10,
-    n_steps=1000000,
-    n_steps_per_epoch=10000,
-    scorers={
-        'environment': d3rlpy.metrics.evaluate_on_environment(env),
-        #'td_error': d3rlpy.metrics.td_error_scorer,
-    },
-    save_interval=10,
-)
+    if args.seed is not None:
+        set_seed(args.seed, env=env)
+    if env is not None:
+        env.seed(args.seed_eval)
 
-print('training finished!')
+    # prepare algorithm
+    cql = d3rlpy.algos.DiscreteCQL(
+        learning_rate=args.learning_rate,
+        n_frames=args.stack_frames,
+        batch_size=args.batch_size,
+        target_update_interval=args.target_update_interval,
+        q_func_factory='qr',
+        scaler='pixel',
+        use_gpu=True,
+    )
+
+    # start training
+    cql.fit(
+        train_episodes,
+        eval_episodes=test_episodes, # in order to trigger evaluations
+        n_steps=args.num_steps,
+        n_steps_per_epoch=args.num_steps_per_epoch,
+        scorers={
+            'environment': d3rlpy.metrics.evaluate_on_environment(env, n_trials=args.eval_episode_num),
+        },
+        save_interval=args.save_interval,
+    )
+
+    print('training finished!')
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('--env_name', required=True)
+    #parser.add_argument('--log_dir', required=True)
+    parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--seed_eval', type=int, default=10000)
+
+    parser.add_argument('--num_steps', type=int, default=1000000, metavar='N',
+                        help='maximum number of training steps (default: 1000000)')
+    parser.add_argument('--num_steps_per_epoch', type=int, default=10000, metavar='N',
+                        help='number of training steps per epoch (default: 10000)')
+    parser.add_argument('--eval_episode_num', type=int, default=10,
+                        help='Number of evaluation episodes (default: 10)')
+    parser.add_argument('--save_interval', type=int, default=10, metavar='N',
+                        help='number of elapsed epochs when saving a model (default: 10)')
+
+    parser.add_argument('--stack_frames', type=int, default=4)                    
+    parser.add_argument('--batch_size', type=int, default=256)
+    parser.add_argument('--learning_rate', type=float, default=6.25e-5)
+    parser.add_argument('--target_update_interval', type=int, default=5000)
+
+    args = parser.parse_args()
+    print(f"args: {args}")
+
+    main(args)
--- run_online.py
+++ run_online.py
@@ -1,32 +1,79 @@
-import d3rlpy
+import random
+import numpy as np
+import torch
 import gym
+import d3rlpy
 from d3rlpy.envs import ChannelFirst
 
-# prepare environment
-env = ChannelFirst(gym.make('Pong-v0')) #('breakout-expert-v0')
-eval_env = ChannelFirst(gym.make('Pong-v0'))
+def set_seed(seed, env=None):
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    if env is not None:
+        env.seed(seed)
 
-# prepare algorithm
-ddqn = d3rlpy.algos.DoubleDQN(
-    n_frames=4,
-    batch_size=256,
-    target_update_interval=5000,
-    q_func_factory='qr',
-    scaler='pixel',
-    use_gpu=True,
-)
+def main(args):
+    # prepare environment
+    env = ChannelFirst(gym.make(args.env_name)) # 'Pong-v0'
+    eval_env = ChannelFirst(gym.make(args.env_name))
 
-# prepare replay buffer
-buffer = d3rlpy.online.buffers.ReplayBuffer(maxlen=1000000, env=env)
+    if args.seed is not None:
+        set_seed(args.seed, env=env)
+    if eval_env is not None:
+        eval_env.seed(args.seed_eval)
 
-# start training
-ddqn.fit_online(
-    env,
-    buffer,
-    n_steps=1000000,
-    n_steps_per_epoch=10000,
-    update_interval=1,
-    eval_env=eval_env,
-    save_interval = 10)
+    # prepare algorithm
+    ddqn = d3rlpy.algos.DoubleDQN(
+        learning_rate=args.learning_rate,
+        n_frames=args.stack_frames,
+        batch_size=args.batch_size,
+        target_update_interval=args.target_update_interval,
+        q_func_factory='qr',
+        scaler='pixel',
+        use_gpu=True,
+    )
 
-print('training finished!')
+    # prepare replay buffer
+    buffer = d3rlpy.online.buffers.ReplayBuffer(maxlen=1000000, env=env)
+
+    # start training
+    ddqn.fit_online(
+        env,
+        buffer,
+        n_steps=args.num_steps,
+        n_steps_per_epoch=args.num_steps_per_epoch,
+        update_interval=1, # update every 1 step
+        eval_env=eval_env, # 10 episodes are evaluated for each epoch. To modify the number episodes to evaluate, it needs to pass the info through this line fit_online()->train_single_env()->evaluate_on_environment()
+        save_interval = args.save_interval,
+    )
+
+    print('training finished!')
+
+if __name__ == '__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('--env_name', required=True)
+    #parser.add_argument('--log_dir', required=True)
+    parser.add_argument('--seed', type=int, default=0)
+    parser.add_argument('--seed_eval', type=int, default=10000)
+
+    parser.add_argument('--num_steps', type=int, default=1000000, metavar='N',
+                        help='maximum number of training steps (default: 1000000)')
+    parser.add_argument('--num_steps_per_epoch', type=int, default=10000, metavar='N',
+                        help='number of training steps per epoch (default: 10000)')
+    parser.add_argument('--eval_episode_num', type=int, default=10,
+                        help='Number of evaluation episodes (default: 10)')
+    parser.add_argument('--save_interval', type=int, default=10, metavar='N',
+                        help='number of elapsed epochs when saving a model (default: 10)')
+
+    parser.add_argument('--stack_frames', type=int, default=4)                    
+    parser.add_argument('--batch_size', type=int, default=256)
+    parser.add_argument('--learning_rate', type=float, default=6.25e-5)
+    parser.add_argument('--target_update_interval', type=int, default=5000)
+
+    args = parser.parse_args()
+    print(f"args: {args}")
+
+    main(args)
